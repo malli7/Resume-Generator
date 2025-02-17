64d37c458755416c4d0aced0ccda7d9b79a14eea
Malli7 <mallireddy2001@gmail.com>
2025-02-17 15:14:44 -0600

    Fix Selenium and Headless Chrome for Railway

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn -w 4 -b 0.0.0.0:$PORT app:app
+web: bash install_packages.sh && gunicorn -w 4 -b 0.0.0.0:$PORT app:app
---
+++ install_packages.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+apt-get update && apt-get install -y \
+    unzip \
+    wget \
+    curl \
+    libglib2.0-0 \
+    libnss3 \
+    libgconf-2-4 \
+    libfontconfig1 \
+    libxrender1
